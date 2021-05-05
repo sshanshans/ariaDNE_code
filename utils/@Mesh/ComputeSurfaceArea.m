410491a1e3a0f334ef651ee7583ec027bd98ee8d
Shan Shan <sshan@math.duke.edu>
2021-05-05 09:29:20 +0200

    Update ComputeSurfaceArea.m
    Force code to compute triangle areas of all size. Addresses issue of zero surface area for tiny mesh

--- utils/@Mesh/ComputeSurfaceArea.m
+++ utils/@Mesh/ComputeSurfaceArea.m
@@ -7,7 +7,8 @@ L2 = sqrt(sum((V(F(:,1),:)-V(F(:,3),:)).^2,2));
 L3 = sqrt(sum((V(F(:,1),:)-V(F(:,2),:)).^2,2));
 S=(L1+L2+L3)/2;
 temp = S.*(S-L1).*(S-L2).*(S-L3);
-temp(temp<eps) = 0;
+% uncomment the following line to get rid of really small triangles
+%temp(temp<eps) = 0;
 TriArea=sqrt(temp);
 Area=sum(TriArea);
 
